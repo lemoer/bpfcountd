345c05f6ad3af1eb82c76c2528796ac0a1b56789
Linus Lüssing <linus.luessing@c0d3.blue>
2020-12-20 20:50:45 +0100

    filter: introducing rule substitutions via ${RULENAME}
    This allows to reuse a previously defined filter. For instance like:
    
    MDNS;udp port 5353
    MDNS4;ip && ${MDNS}
    MDNS6;ip6 && ${MDNS}
    
    Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>

--- util.c
+++ util.c
@@ -23,6 +23,7 @@ void get_mac(char *dest, const char *iface) {
 }
 
 void strnrepl(const char *token, const char *replace, char *str, size_t n) {
+	const size_t str_br_len = strlen("()");
 	char *ptr = str;
 	char *p_behind;
 	size_t length_new = strlen(str);
@@ -36,15 +37,24 @@ void strnrepl(const char *token, const char *replace, char *str, size_t n) {
 			return;
 
 		// calculate the new length and check for overflow
-		length_new += strlen(replace) - strlen(token);
+		length_new += strlen(replace) - strlen(token) + str_br_len;
 		if (length_new >= n)
 			return;
 
 		// this points to the position behind the token
 		p_behind = ptr + strlen(token);
 
-		memmove(ptr + strlen(replace), p_behind, strlen(p_behind));
+		// making room for replacement
+		memmove(ptr + strlen(replace) + str_br_len, p_behind,
+			strlen(p_behind));
+
+		// inserting replacement
+		*ptr = '(';
+		ptr += 1;
 		memcpy(ptr, replace, strlen(replace));
+		ptr += strlen(replace);
+		*ptr = ')';
+
 		str[length_new] = 0x00;
 	}
 }
